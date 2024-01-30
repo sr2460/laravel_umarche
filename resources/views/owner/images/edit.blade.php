470f21211aa433f59d6fc41934e71a3edc761d5e
sr2460 <shiningrosd2460@gmail.com>
2024-01-31 00:56:19 +0900

    edit, update

--- resources/views/owner/images/edit.blade.php
+++ resources/views/owner/images/edit.blade.php
@@ -17,9 +17,17 @@
                           <div class="p-2 w-1/2 mx-auto">
                             <div class="relative">
                               <label for="title" class="leading-7 text-sm text-gray-600">画像タイトル</label>
-                              <input type="text" id="title" name="title" value="{{$image->$title}}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                              <input type="text" id="title" name="title" value="{{$image->title}}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                             </div>
                           </div>
+                          <div class="p-2 w-1/2 mx-auto">
+                            <div class="relative">
+                            <div class="w-32">
+                            <x-thumnail :filename="$image->filename" type="products" />
+                          </div>
+                        </div>
+                      </div>
+                      
                           <div class="p-2 w-full justify-around mt-4">
                             <button type="button" onclick="location.href='{{ route('owner.images.index')}}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
                             <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button> 
