e9f6e57d327c6bf8ee26c6935360c8a76eed95ae
sr2460 <shiningrosd2460@gmail.com>
2024-02-14 21:12:57 +0900

    ショップ情報

--- resources/views/components/select-image.blade.php
+++ resources/views/components/select-image.blade.php
@@ -15,7 +15,8 @@ $cId = $currentId ?? '' ;
     <div class="modal__overlay z-50" tabindex="-1" data-micromodal-close>
       <div class="modal__container" role="dialog" aria-modal="true" aria-labelledby="{{$modal}}-title">
         <header class="modal__header">
-          <h2 text-xl= text-xl text = gray-700 class="modal__title" id="{{$modal}}">
+          <!-- <h2 text-xl= text-xl text = gray-700 class="modal__title" id="{{$modal}}">-->
+          <h2 class= "text-xl text-gray-700" id="{{$modal}}-title">
             ファイルを選択してください
           </h2>
           <button type="button" class ="modal__close" aria-label="Close modal" data-micromodal-close></button>
